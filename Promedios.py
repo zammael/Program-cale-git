1d2f33a41ebb09382a9db153421c076830d9ec37
Samuelseg <eliud-segovia@hotmail.com>
2023-12-13 01:53:20 -0700

    Ahora es una funcion

--- Promedios.py
+++ Promedios.py
@@ -1,10 +1,12 @@
 
-
-i = int(input ("Cuantos valores desea introducir? "))
-res = 0
-for x in range(i):
-    nums = int(input (f"Introduzca el valor numero {x+1}: "))
-    res = res + nums
+def promi():
+    i = int(input ("Cuantos valores desea introducir? "))
+    res = 0
+    for x in range(i):
+        nums = int(input (f"Introduzca el valor numero {x+1}: "))
+        res = res + nums
     
-print("El promedio de los valores es: ")
-print(res/i)
+    print("El promedio de los valores es: ")
+    print(res/i)
+
+promi()
