789666ea8951d025f52a1a1323549d7e1b6c593b
Samuelseg <eliud-segovia@hotmail.com>
2023-12-13 03:04:28 -0700

    Ahora la funcion usa parametros y argumentos

--- Promedios.py
+++ Promedios.py
@@ -1,12 +1,15 @@
 
-def promi():
-    i = int(input ("Cuantos valores desea introducir? "))
+nums = []
+
+i = int(input ("Cuantos valores desea introducir? "))
+for x in range(i):
+    nums.append(int(input (f"Introduzca el valor numero {x+1}: ")))
+
+def average(vals):
     res = 0
-    for x in range(i):
-        nums = int(input (f"Introduzca el valor numero {x+1}: "))
-        res = res + nums
-    
+    for x in vals:
+        res = res + x
     print("El promedio de los valores es: ")
     print(res/i)
 
-promi()
+average(nums)
