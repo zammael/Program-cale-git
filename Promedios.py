75475cbb0546b343454085d224541a0cac6271d8
Eliud Segovia <eliud-segovia@hotmail.com>
2023-12-14 01:05:11 -0600

    fixed & da la posicion en la cadena

--- Promedios.py
+++ Promedios.py
@@ -13,25 +13,38 @@ def average(vals):
     print(res/i)
 
 def big(vals):
-    x = 0
-    y = 0
-    for x in vals:
-        if x > y:
-            y = x
-    print("El numero mas grande de los valores es: ")
-    print(y)
-
-def small(vals):
-    x = 0
     y = 0
     z = 0
+    p = 0
     for x in vals:
-        if y == z:
+        z += 1
+        if z == 1:
             y = x
+            p = z
+        if x > y:
+            y = x
+            p = z    # p se utiliza para tomar el valor del contador z cuando se haya encontrado el numero
+    print("El numero mas grande de los valores es: ")
+    print(y)
+    print("Y su posicion es: ")
+    print(p)
+
+def small(vals):
+    y = 0
+    z = 0
+    p = 0
+    for x in vals:
+        z += 1
+        if z == 1:
+            y = x
+            p = z
         if x < y:
             y = x
+            p = z
     print("El numero mas pequeño de los valores es: ")
     print(y)
+    print("Y su posicion es: ")
+    print(p)
 
 average(nums)
 big(nums)
